e357819a6233e7002f495ce76e5a6394687189d2
julien <julien.raynal@yahoo.fr>
2020-12-24 18:09:09 +0100

    linux

--- gen_config.sh
+++ gen_config.sh
@@ -1,4 +1,2 @@
 # Little script to simplify configuration file generating template.
-python3 -c "import os; print(os.getcwd())"
-python3 -c "import sys, os; sys.path.append(os.getcwd())"
-python3 utils/generate_config_file.py
+python3 generate_config_file.py
