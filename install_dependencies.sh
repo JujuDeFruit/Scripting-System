46e4d5d833087c63992942bf21e55f6ea306afd2
julien <julien.raynal@yahoo.fr>
2020-12-25 02:44:28 +0100

    Cron

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,7 +8,7 @@ python3 --version
 
 # Install Pip if it is not installed yet.
 # Pip is a dependencies manager. It simplifies libraries imports.
-apt-get install python3-pip
+sudo apt-get install python3-pip
 
 # Check pip is correctly installed.
 python3 -m pip --version
@@ -21,11 +21,17 @@ python3 -m pip install email_validator
 python3 -m pip install pysftp
 
 # Create crontab and make archival automatic every day.
-touch /var/spool/cron/crontabs/archival
+{ crontab -l -u $USER; echo "0 0 * * * cd $PWD; python3 main.py"; } | crontab -u $USER -
 
 # Configure crontab to call main.py everyday at midnight.
 # We suppose web server is launched 
-# and we do not need to launch it manually
-echo "0 0 * * * julien  python3 "$PWD"/main.py" > /var/spool/cron/crontabs/archival
+# and we do not need to launch it manually.
+# Each day at midnight (0 0) go to PWD (Scripting-System folder)
+# and execute main.py. It is written in good crontab.
 
-echo "Successfully 'archival' crontab created in /var/spool/cron/crontabs"
+echo "Successfully crontab created in /var/spool/cron/crontabs."
+echo "Logs for this crontab are in /var/log/syslog."
+echo "To restart service, type sudo service cron restart/start."
+
+# Launch cron service
+sudo service cron start
