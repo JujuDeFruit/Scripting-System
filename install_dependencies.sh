70a19b3f4daaa4131d6f15406e45c073252b0f5e
julien <julien.raynal@yahoo.fr>
2020-12-25 01:21:34 +0100

    conflict

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,7 +8,8 @@ python3 --version
 
 # Install Pip if it is not installed yet.
 # Pip is a dependencies manager. It simplifies libraries imports.
-sudo apt-get install python3-pip
+apt-get install python3-pip
+
 # Check pip is correctly installed.
 python3 -m pip --version
 
@@ -18,3 +19,13 @@ python3 -m pip --version
 
 python3 -m pip install email_validator
 python3 -m pip install pysftp
+
+# Create crontab and make archival automatic every day.
+touch /var/spool/cron/crontabs/archival
+
+# Configure crontab to call main.py everyday at midnight.
+# We suppose web server is launched 
+# and we do not need to launch it manually
+echo "0 0 * * * julien  python3 "$PWD"/main.py" > /var/spool/cron/crontabs/archival
+
+echo "Successfully 'archival' crontab created in /var/spool/cron/crontabs"
